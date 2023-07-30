a36f00a83ab643b97516eae1ee36c4146349bfba
abigail-143 <abigailtoh1403@gmail.com>
2023-07-30 15:46:28 +0800

    created mainpage, navbar, header, footer and app primary layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+// the react-route-dom is in the DevDependecies (package.json) do i need to move it up to dependencies?
+
+import Navbar from "./components/Navbar";
+import MainPage from "./pages/MainPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <h2>GA SEI</h2>
+      <Header></Header>
+      <Navbar></Navbar>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/main" />}></Route>
+        </Routes>
+      </Suspense>
+      <Footer></Footer>
     </div>
   );
 }
 
 export default App;
+
+// Header />
+// Navbar/>
+// Suspense / Routes / Route > each link to a new page
+// Footer/>
