8c7bfe2e68f54027657e8a1e491b4039ed29d515
abigail-143 <abigailtoh1403@gmail.com>
2023-07-30 16:36:56 +0800

    extracted navbar details from met api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    
+  const [departments, setDepartments] = useState([]);
+
+  const getDepartments = async () => {
+    const res = await fetch(import.meta.env.VITE_SERVER + "/departments");
+    const data = await res.json();
+    const array = data.departments;
+    setDepartments(array);
+  };
+
+  useEffect(() => {
+    getDepartments();
+  }, []);
+
+  const departmentLinks = departments.map((item) => {
+    const linkName = "/department-" + item.departmentId;
+    return (
+      <li>
+        <NavLink exact to={linkName}>
+          {item.displayName}
+        </NavLink>
+      </li>
+    );
+  });
+
   return (
     <div>
       <h1>Navbar</h1>
-      <nav>
+      <nav className="navbar">
         <ul>
           <li>
             <NavLink exact to="/main">
               Main Page
             </NavLink>
           </li>
+          {departmentLinks}
         </ul>
       </nav>
+      {/* <p>{JSON.stringify(departments)}</p> */}
     </div>
   );
 };
