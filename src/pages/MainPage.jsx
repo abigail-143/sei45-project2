3f6e46b8d5e9b70f5d2832704970caa415402c28
abigail-143 <abigailtoh1403@gmail.com>
2023-07-31 13:47:23 +0800

    trying to extract data from array but it keeps running

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,20 +2,50 @@ import React, { useEffect, useState } from "react";
 import Artwork from "../components/Artwork";
 
 const MainPage = () => {
-  const [testpage, setTestPage] = useState([]);
+  // this works to get the array of unique objectIds from a specific department
+  //   const [testpage, setTestPage] = useState([]);
 
-  const getObjects = async () => {
-    const res = await fetch(
-      import.meta.env.VITE_SERVER + "/objects?departmentIds=1"
-    );
-    const data = await res.json();
-    const array = data.objectIDs;
-    setTestPage(array);
-  };
+  //   const getObjects = async () => {
+  //     const res = await fetch(
+  //       import.meta.env.VITE_SERVER + "/objects?departmentIds=1"
+  //     );
+  //     const data = await res.json();
+  //     const array = data.objectIDs;
+  //     setTestPage(array);
+  //   };
 
-  useEffect(() => {
-    getObjects();
-  }, []);
+  //   useEffect(() => {
+  //     getObjects();
+  //   }, []);
+  //------------------------------------------//
+
+    const testArray = [180, 183, 184, 185, 10];
+
+  //   const [testObjectData, setTestObjectData] = useState([]);
+
+  //   const getObjectDetails = async (objectId) => {
+  //     const res = await fetch(
+  //       import.meta.env.VITE_SERVER + "/objects/" + objectId
+  //     );
+  //     const data = await res.json();
+  //     setTestObjectData(data);
+  //   };
+
+    for (let i = 0; i < testArray.length; i++) {
+    const [testObjectData, setTestObjectData] = useState([]);
+
+    const getObjectDetails = async (objectId) => {
+      const res = await fetch(
+        import.meta.env.VITE_SERVER + "/objects/" + objectId
+      );
+      const data = await res.json();
+      setTestObjectData(data);
+    };
+      const data = getObjectDetails(testArray[i]);
+      if (data.isPublicDomain) {
+        return data.primaryImage;
+      }
+    }
 
   return (
     <div className="department-page">
@@ -23,7 +53,8 @@ const MainPage = () => {
       <div className="department-art-display">
         <Artwork></Artwork>
       </div>
-      <div>{JSON.stringify(testpage)}</div>
+      {/* <div>{JSON.stringify(testpage)}</div> */}
+      {/* <div>{JSON.stringify(testObjectData)}</div> */}
     </div>
   );
 };
