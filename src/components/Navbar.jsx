a8539a7c1b1f3c815bf2127a8bdd16c850d4a93f
abigail-143 <abigailtoh1403@gmail.com>
2023-07-31 11:24:12 +0800

    updated Navbar component and created first department component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <h1>Navbar</h1>
+      {/* <h1>Navbar</h1> */}
       <nav className="navbar">
         <ul>
           <li>
---
+++ src/pages/AmericanDecorativeArts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AmericanDecorativeArts = () => {
+    return (
+        <div>
+            <h1>American Decorative Arts</h1>
+        </div>
+    );
+};
+
+export default AmericanDecorativeArts;
