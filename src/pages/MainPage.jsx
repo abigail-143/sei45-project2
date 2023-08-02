c1fb558f2b7d4a9f87c3def4331da2fa97676229
abigail-143 <abigailtoh1403@gmail.com>
2023-08-02 17:36:51 +0800

    edit mainpage to show more details

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,8 +3,23 @@ import React, { useEffect, useState, useRef } from "react";
 const MainPage = () => {
   return (
     <div className="main-page text-center">
-      <h1>Visit The Met</h1>
-
+      <div className="write-up">
+        <h1>The Metropolitan Museum of Art</h1>
+        <p>
+          The Metropolitan Museum of Art presents over 5,000 years of art from
+          around the world for everyone to experience and enjoy.
+        </p>
+        <p>
+          This is a digital gallery of a subset of the open source data made
+          available through The Metropolitan Museum of Art Collection API.
+        </p>
+        <p>
+          For more information on the API, visit{" "}
+          <a href="https://metmuseum.github.io/">The Met's github</a>. For a
+          visit to the museum, plan your visit with{" "}
+          <a href="https://www.metmuseum.org/">them</a>.
+        </p>
+      </div>
     </div>
   );
 };
