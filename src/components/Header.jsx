98b5ff33a258d8b10f009c6587a1cbc14d32c111
abigail-143 <abigailtoh1403@gmail.com>
2023-08-02 18:31:14 +0800

    amended logo src

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = (props) => {
   return (
     <div className="header">
-      <img className="logo" src="../public/METlogo.png" alt="logo" />
+      <img className="logo" src="/METlogo.png" alt="logo" />
       <h1>The Met Museum: <br/>A Digital Gallery</h1>
       <h2 className="header-dept-name">{props.pageChange}</h2>
     </div>
